7597ac95c9e466d88f7450f09e35917fe50b72db
Nightrix-0 <Loganchubbs3@hotmail.com>
2026-09-03 19:51:14 -0600

    Update ReportAdminPlugin.java

--- src/main/java/net/nightrix/reportadmin/ReportAdminPlugin.java
+++ src/main/java/net/nightrix/reportadmin/ReportAdminPlugin.java
@@ -1,10 +1,12 @@
 package net.nightrix.reportadmin;
 
 import net.nightrix.reportadmin.commands.AdminRequestCommands;
+import net.nightrix.reportadmin.commands.LogsCommand;
 import net.nightrix.reportadmin.commands.ReportCommands;
 import net.nightrix.reportadmin.gui.PlayerSelectMenu;
 import net.nightrix.reportadmin.storage.AdminRequestManager;
 import net.nightrix.reportadmin.storage.ReportManager;
+import net.nightrix.reportadmin.storage.TicketLogManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +14,7 @@ public class ReportAdminPlugin extends JavaPlugin {
 
     private ReportManager reportManager;
     private AdminRequestManager adminRequestManager;
+    private TicketLogManager ticketLogManager;
 
     @Override
     public void onEnable() {
@@ -21,21 +24,26 @@ public class ReportAdminPlugin extends JavaPlugin {
 
         this.reportManager = new ReportManager(this);
         this.adminRequestManager = new AdminRequestManager(this);
+        this.ticketLogManager = new TicketLogManager(this);
 
         PlayerSelectMenu playerSelectMenu = new PlayerSelectMenu(this);
         Bukkit.getPluginManager().registerEvents(playerSelectMenu, this);
 
-        ReportCommands reportCommands = new ReportCommands(reportManager, playerSelectMenu);
+        ReportCommands reportCommands = new ReportCommands(reportManager, playerSelectMenu, ticketLogManager);
         getCommand("flag").setExecutor(reportCommands);
         getCommand("myreports").setExecutor(reportCommands);
         getCommand("reportlist").setExecutor(reportCommands);
 
-        AdminRequestCommands adminRequestCommands = new AdminRequestCommands(adminRequestManager);
+        AdminRequestCommands adminRequestCommands = new AdminRequestCommands(adminRequestManager, ticketLogManager);
         getCommand("requestadmin").setExecutor(adminRequestCommands);
         getCommand("myadminrequests").setExecutor(adminRequestCommands);
         getCommand("adminrequests").setExecutor(adminRequestCommands);
 
-        getLogger().info("ReportAdmin enabled: /flag, /myreports, /reportlist (alias /reports), /requestadmin, /myadminrequests, /adminrequests");
+        LogsCommand logsCommand = new LogsCommand(ticketLogManager);
+        getCommand("ralogs").setExecutor(logsCommand);
+
+        getLogger().info("ReportAdmin enabled: /flag, /myreports, /reportlist (alias /reports), "
+                + "/requestadmin, /myadminrequests, /adminrequests, /ralogs");
     }
 
     @Override
@@ -45,6 +53,9 @@ public class ReportAdminPlugin extends JavaPlugin {
         }
         if (adminRequestManager != null) {
             adminRequestManager.save();
+        }
+        if (ticketLogManager != null) {
+            ticketLogManager.save();
         }
     }
 }
